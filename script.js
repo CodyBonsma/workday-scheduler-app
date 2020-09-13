81d26ac52e15219bc7f9e3e4ca8186639be18d9c
Cody Bonsma <codybonsma@gmail.com>
2020-09-12 22:23:16 -0400

    worked on the hour logic and compare the AM PM times to check current time

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var currentDayEl = $("<h2>");
+  currentDayEl.text(moment().format("dddd, MMMM Do YYYY"));
+  $("#currentDay").append(currentDayEl);
 
-      var currentDayEl = $("<h2>");
-      currentDayEl.text(moment().format("dddd, MMMM Do YYYY"));
-      $("#currentDay").append(currentDayEl);
+  var currentHour = parseInt(moment().format("h"));
+  console.log(currentHour);
 
-      var currentHour = moment().format("hA");
-      console.log(currentHour);
-
-// create time blocks for standard business hours 9am - 5pm (9blocks)
-// each block should be a row and have a col 1 (hour) col 10 (text) col 1 (lock)
-
-// variable array with the different time slots 
-var hourId = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  var currentHourTag = moment().format("hA");
 
 
+  // create time blocks for standard business hours 9am - 5pm (9blocks)
+  // each block should be a row and have a col 1 (hour) col 10 (text) col 1 (lock)
 
-for(var i = 0; i < hourId.length; i++){
-//create the main row div 
+  // variable array with the different time slots
+  var hourId = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
+  ];
+
+  for (var i = 0; i < hourId.length; i++) {
+    //create the main row div
     var rowEl = $("<row>");
     rowEl.addClass("main-row");
     rowEl.addClass(hourId[i]);
@@ -34,28 +44,62 @@ for(var i = 0; i < hourId.length; i++){
     rowEl.append(textEl);
     //create the save button div element col-sm-1
     var saveEl = $("<div>");
-    saveEl.addClass("saveBtn time-block col-sm-1");
+    saveEl.addClass("saveBtn time-block col-sm-1 text-area");
     saveEl.attr("id", hourId[i]);
-    saveEl.attr("style", "float")
+    saveEl.attr("style", "float");
     saveEl.text("BUTTON");
     saveEl.click(save);
     rowEl.append(saveEl);
 
+    console.log(parseInt(hourId[i].slice(0, -2)));
 
-
-}
-
+    if (compareTime(hourId[i], currentHourTag) === 0) {
+      textEl.addClass("present");
+    } else if (compareTime(hourId[i], currentHourTag) > 0) {
+      textEl.addClass("past");
+    } else if (compareTime(hourId[i], currentHourTag) < 0) {
+      textEl.addClass("future");
+    }
+  }
+  // parseInt(hourId[i].slice(0, -2)) === currentHour
 });
 
+function compareTime(timeOne, timeTwo) {
+  if (timeOne.slice(-2) === "PM") {
+    if (parseInt(timeOne.slice(0, -2)) === 12) {
+      timeOne = parseInt(timeOne.slice(0, -2));
+    } else {
+      timeOne = parseInt(timeOne.slice(0, -2) + 12);
+    }
+  } else {
+      if (parseInt(timeOne.slice(0, -2)) === 12){
+        timeOne = parseInt(timeOne.slice(0, -2) + 12);
+      } else {
+        timeOne = parseInt(timeOne.slice(0, -2));
+      }
+  }
+  if (timeTwo.slice(-2) === "PM") {
+    if (parseInt(timeTwo.slice(0, -2)) === 12) {
+      timeOne = parseInt(timeTwo.slice(0, -2));
+    } else {
+      timeTwo = parseInt(timeTwo.slice(0, -2) + 12);
+    }
+  } else {
+    if (parseInt(timeTwo.slice(0, -2)) === 12){
+        timeTwo = parseInt(timeTwo.slice(0, -2) + 12);
+      } else {
+        timeTwo = parseInt(timeTwo.slice(0, -2));
+      }
+  }
+  return timeOne - timeTwo;
+}
 
+// on click,save - start function to save user input on local storage
 
-// on click,save - start function to save user input on local storage 
-
-
-function save (){
-  var saveHourId =  $(this).attr("id");
-console.log(saveHourId);
-var saveText = $("#" + saveHourId).val();
-console.log(saveText);
-localStorage.setItem( saveHourId, saveText); 
+function save() {
+  var saveHourId = $(this).attr("id");
+  console.log(saveHourId);
+  var saveText = $("#" + saveHourId).val();
+  console.log(saveText);
+  localStorage.setItem(saveHourId, saveText);
 }
